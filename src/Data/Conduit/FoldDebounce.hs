5a710c54075ebe442715a138871abc72688247de
Toshio Ito <debug.ito@gmail.com>
2015-06-16 21:29:49 +0900

    import signature of 'debounceSource' from fold-debounce repository. Pass compile.

--- src/Data/Conduit/FoldDebounce.hs
+++ src/Data/Conduit/FoldDebounce.hs
@@ -7,4 +7,28 @@ module Data.Conduit.FoldDebounce (
   
 ) where
 
+import Prelude hiding (init)
+import Control.Applicative (Applicative)
+import Control.Monad (void)
+
+import Control.FoldDebounce (Args(Args,cb,fold,init),
+                             Opts, delay, alwaysResetTimer, def)
+import Data.Conduit (Source, Sink, await, yield, ($$))
+import Control.Monad.Base (MonadBase)
+import Control.Monad.Trans.Resource (MonadBaseControl, MonadThrow, 
+                                     allocate, register, resourceForkIO, runResourceT)
+import Control.Monad.Trans.Class (lift)
+import Control.Monad.IO.Class (MonadIO, liftIO)
+import Control.Concurrent.STM (newTChanIO, writeTChan, readTChan, atomically)
+
+import Control.Concurrent (threadDelay)
+import qualified Data.Conduit.List as CL
+
+-- | 
+debounceSource :: (MonadThrow m, MonadBase IO m, MonadIO m, Applicative m, MonadBaseControl IO m)
+                  => Args i o -- ^ mandatory argument for FoldDebounce
+                  -> Opts i o -- ^ optional argument for FoldDebounce
+                  -> Source m i -- ^ original 'Source'
+                  -> Source m o -- ^ debounced 'Source'
+debounceSource = undefined
 
