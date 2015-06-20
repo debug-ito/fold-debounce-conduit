22fba9b8830c00e953dc0ee27945b15675f6beed
Toshio Ito <debug.ito@gmail.com>
2015-06-20 18:31:30 +0900

    Add test when the original Source terminates immediately.

--- test/Data/Conduit/FoldDebounceSpec.hs
+++ test/Data/Conduit/FoldDebounceSpec.hs
@@ -4,6 +4,7 @@ import Test.Hspec
 
 import Data.Monoid (Monoid)
 import Control.Concurrent (threadDelay)
+import System.Timeout (timeout)
 import qualified Data.Conduit.FoldDebounce as F
 import Data.Conduit (Source, ($$), yield)
 import Control.Monad.IO.Class (liftIO)
@@ -38,3 +39,6 @@ spec = do
       let s = delayedSource [(1000, "a"), (1000, "b"), (200000, "c"), (1000, "d"), (1000, "e"), (200000, "f")]
       ret <- debMonoid 100000 s $$ CL.consume
       ret `shouldBe` ["ab", "cde", "f"]
+    it "should terminate debounced Source immediately if the original Source terminates immediately" $ do
+      ret <- timeout 50000000 $ debMonoid 60000000 (CL.sourceList ["A", "B", "C", "D", "E"]) $$ CL.consume
+      ret `shouldBe` Just ["ABCDE"]
