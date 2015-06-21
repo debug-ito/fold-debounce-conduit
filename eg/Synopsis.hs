31cba63271143d323cfaa43b5e11ae22eab8a3b4
Toshio Ito <debug.ito@gmail.com>
2015-06-21 18:25:15 +0900

    revise the Synopsis. It's easier to understand this module to fold into a list than it is to fold to sum.

--- eg/Synopsis.hs
+++ eg/Synopsis.hs
@@ -1,6 +1,6 @@
 module Main (main) where
 
-import Data.Conduit (Source, Sink, yield, await, ($$), (=$))
+import Data.Conduit (Source, Sink, yield, ($$))
 import qualified Data.Conduit.List as CL
 import Control.Concurrent (threadDelay)
 import Control.Monad.IO.Class (liftIO)
@@ -8,23 +8,26 @@ import Control.Monad.Trans.Resource (ResourceT, runResourceT)
 
 import qualified Data.Conduit.FoldDebounce as F
 
-fastSource :: Source (ResourceT IO) Int
-fastSource = fastStream' 0 where
+fastSource :: Int -> Source (ResourceT IO) Int
+fastSource max_num = fastStream' 0 where
   fastStream' count = do
     yield count
-    liftIO $ threadDelay 100000
-    fastStream' (count + 1)
+    if count >= max_num
+      then return ()
+      else do
+        liftIO $ threadDelay 100000
+        fastStream' (count + 1)
 
-printSink :: Int -> Sink Int (ResourceT IO) ()
-printSink num = CL.isolate num =$ CL.mapM_ (liftIO . putStrLn . show)
+printSink :: Show a => Sink a (ResourceT IO) ()
+printSink = CL.mapM_ (liftIO . putStrLn . show)
 
 main :: IO ()
 main = do
   putStrLn "-- Before debounce"
-  runResourceT $ fastSource $$ printSink 20
+  runResourceT $ fastSource 20 $$ printSink
   let debouncer = F.debounce F.Args { F.cb = undefined, -- not used
-                                      F.fold = (+),
-                                      F.init = 0 }
+                                      F.fold = (\list num -> list ++ [num]),
+                                      F.init = [] }
                              F.def { F.delay = 500000 }
   putStrLn "-- After debounce"
-  runResourceT $ debouncer fastSource $$ printSink 20
+  runResourceT $ debouncer (fastSource 20) $$ printSink
