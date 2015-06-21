7f73906a70e9749c374ca9694a14eae5ce3905bf
Toshio Ito <debug.ito@gmail.com>
2015-06-21 18:18:48 +0900

    eg/Synopsis: use Control.Conduit.List to shorten the code.

--- eg/Synopsis.hs
+++ eg/Synopsis.hs
@@ -1,34 +1,30 @@
 module Main (main) where
 
-import Data.Conduit (Source, Sink, yield, await, ($$))
+import Data.Conduit (Source, Sink, yield, await, ($$), (=$))
+import qualified Data.Conduit.List as CL
 import Control.Concurrent (threadDelay)
 import Control.Monad.IO.Class (liftIO)
+import Control.Monad.Trans.Resource (ResourceT, runResourceT)
 
 import qualified Data.Conduit.FoldDebounce as F
 
-fastSource :: Source IO Int
+fastSource :: Source (ResourceT IO) Int
 fastSource = fastStream' 0 where
   fastStream' count = do
     yield count
     liftIO $ threadDelay 100000
     fastStream' (count + 1)
 
-printSink :: Int -> Sink Int IO ()
-printSink end_count = do
-  mgot <- await
-  case mgot of
-    Nothing -> return ()
-    Just got -> do
-      liftIO $ putStrLn (show got)
-      if got >= end_count
-        then return ()
-        else printSink end_count
+printSink :: Int -> Sink Int (ResourceT IO) ()
+printSink num = CL.isolate num =$ CL.mapM_ (liftIO . putStrLn . show)
 
 main :: IO ()
 main = do
-  fastSource $$ printSink 20
+  putStrLn "-- Before debounce"
+  runResourceT $ fastSource $$ printSink 20
   let debouncer = F.debounce F.Args { F.cb = undefined, -- not used
                                       F.fold = (+),
                                       F.init = 0 }
                              F.def { F.delay = 500000 }
-  debouncer fastSource $$ printSink 20
+  putStrLn "-- After debounce"
+  runResourceT $ debouncer fastSource $$ printSink 20
