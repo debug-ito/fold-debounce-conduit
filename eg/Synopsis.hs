79ea93000dd89ee9b8b7af9754493eebfc22f5cf
Toshio Ito <debug.ito@gmail.com>
2018-02-03 18:39:08 +0900

    update document.
    remove deprecated symbols.

--- eg/Synopsis.hs
+++ eg/Synopsis.hs
@@ -1,14 +1,15 @@
 module Main (main) where
 
-import Data.Conduit (Source, Sink, yield, ($$))
+import Data.Conduit (ConduitT, yield, runConduit, (.|))
 import qualified Data.Conduit.List as CL
+import Data.Void (Void)
 import Control.Concurrent (threadDelay)
 import Control.Monad.IO.Class (liftIO)
 import Control.Monad.Trans.Resource (ResourceT, runResourceT)
 
 import qualified Data.Conduit.FoldDebounce as F
 
-fastSource :: Int -> Source (ResourceT IO) Int
+fastSource :: Int -> ConduitT () Int (ResourceT IO) ()
 fastSource max_num = fastStream' 0 where
   fastStream' count = do
     yield count
@@ -18,16 +19,16 @@ fastSource max_num = fastStream' 0 where
         liftIO $ threadDelay 100000
         fastStream' (count + 1)
 
-printSink :: Show a => Sink a (ResourceT IO) ()
+printSink :: Show a => ConduitT a Void (ResourceT IO) ()
 printSink = CL.mapM_ (liftIO . putStrLn . show)
 
 main :: IO ()
 main = do
   putStrLn "-- Before debounce"
-  runResourceT $ fastSource 10 $$ printSink
+  runResourceT $ runConduit $ fastSource 10 .| printSink
   let debouncer = F.debounce F.Args { F.cb = undefined, -- anything will do
                                       F.fold = (\list num -> list ++ [num]),
                                       F.init = [] }
                              F.def { F.delay = 500000 }
   putStrLn "-- After debounce"
-  runResourceT $ debouncer (fastSource 10) $$ printSink
+  runResourceT $ runConduit $ debouncer (fastSource 10) .| printSink
